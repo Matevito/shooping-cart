21a6e36add437c34ac1ee74071ef00547ebba9ff
Mateo <madiazt@unal.edu.co>
2021-06-14 18:39:34 -0500

    added beta logic of rendering products cards in store page

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from "react"
+import ProductCard from "./itemsComponents/ProductCard"
 
 const Store = () => {
     const [productsData, set_productsData] = useState("")
 
     useEffect(() => {
-        fetchStoreData()
-        console.log(productsData)
+        fetchStoreData()  
     }, [])
 
     const fetchStoreData = () => {
-        fetch("https://fakestoreapi.com/products")
+        fetch("https://fakestoreapi.com/products", {mode:"cors"})
             .then(response => response.json())
             .then(storeData => set_productsData(storeData))
     }
     return(
         <div className="row">
-            <span>store</span>
+            <h1 className="text-center">Store products</h1>
+            {/* Render the cards*/}
+            {productsData.map(product => {
+                return <ProductCard key={product.id} 
+                product_data={product}/>
+            })}
         </div>
     )
 }
---
+++ src/components/itemsComponents/ProductCard.js
@@ -0,0 +1,12 @@
+
+
+const ProductCard = () => {
+
+    return(
+        <div>
+            a product
+        </div>
+    )
+}
+
+export default ProductCard
