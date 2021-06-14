34c22d0f93bd228d9369dfed6211a0b45dddc94a
Mateo <madiazt@unal.edu.co>
2021-06-14 13:20:56 -0500

    app jsx skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      Hello world
+    <div className="conteiner-flex">
+      <Router>
+        <Navbar />
+
+        <Switch>
+          {/*Here go the app routes*/}
+        </Switch>
+        
+        <Footer />
+      </Router>
     </div>
   );
 }
