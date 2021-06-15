5eb2b0a02dcf628745d5dee33ac0a2e88f7fae0d
Mateo <madiazt@unal.edu.co>
2021-06-14 23:04:40 -0500

    added product fetching logic

--- src/components/itemsComponents/ProductPage.js
+++ src/components/itemsComponents/ProductPage.js
@@ -1,5 +1,20 @@
+import { response } from "express"
+import React, { useEffect, useState } from "react"
 
 const ProductPage = (props) => {
+    const [productData, set_productData] = useState({})
+
+    useEffect(()=> {
+        fetchProductData()
+    }, [])
+
+    const fetchProductData = () => {
+        fetch("https://fakestoreapi.com/products/",{mode:"cors"})
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .then(pData => set_productData(pData))
+    }
+
     return(
         <div>
             product page
